60f6202390fda19f155b237cdc80b13725ead78e
AppSSculpt <146571462+AppSSculpt@users.noreply.github.com>
2023-11-27 21:01:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function createQRCode(qrCodeValue) {
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: qrCodeValue,
+        width: 120,
+        height: 120,
+    });
+}
+
+
+function displayMaterial(materialValue) {
+    document.getElementById("material").textContent = "Material: " + materialValue;
+}
+
+
+function displayCodigo(codigoValue) {
+    document.getElementById("codigo").textContent = "Código: " + codigoValue;
+}
+
+
+function createCustomImageAndDownload() {
+    
+    var container = document.querySelector(".custom-qrcode-container");
+
+    
+    html2canvas(container).then(function(canvas) {
+        
+        var customImage = new Image();
+        customImage.src = canvas.toDataURL("image/png");
+
+        
+        var downloadLink = document.createElement("a");
+        downloadLink.href = customImage.src;
+        downloadLink.download = materialValue + ".png";
+
+        
+        downloadLink.click();
+    });
+}
+
+var downloadButton = document.getElementById("downloadCustomImage");
+if (downloadButton) {
+    downloadButton.addEventListener("click", createCustomImageAndDownload);
+}
+
+
+var urlParams = new URLSearchParams(window.location.search);
+var qrCodeValue = urlParams.get("qr_code");
+var materialValue = urlParams.get("material");
+var codigoValue = urlParams.get("qr_code");
+
+createQRCode(qrCodeValue);
+displayMaterial(materialValue);
+displayCodigo(codigoValue);
+
+
+window.addEventListener("scroll", function() {
+    var h1 = document.querySelector("h1");
+    if (window.scrollY > 0) {
+        h1.style.top = "-100px"; 
+    } else {
+        h1.style.top = "0"; 
+    }
+});
+
+
+document.getElementById('downloadCustomImage').addEventListener('click', function() {
+    var btn = this;
+    btn.classList.add('downloading');
+    setTimeout(function() {
+      btn.classList.remove('downloading');
+    }, 3000); 
+  });
